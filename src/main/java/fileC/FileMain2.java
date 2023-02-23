6511a6bbbe1f3159fc1eb8986a0fe515cf02cb65
LordKazor <LordKazor@KazorCaverna2.mshome.net>
2023-02-23 14:28:11 +0100

    Rev 1.61

--- src/main/java/fileC/FileMain2.java
+++ src/main/java/fileC/FileMain2.java
@@ -1,18 +1,23 @@
 package fileC;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Arrays;
 
 public class FileMain2 {
     public static void main(String[] args) throws IOException{
-   
+    
+    // CREO LA MIA DIRECTORY
     File documentiDir = new File("C:\\Users\\setti\\Documents\\NetBeansProjects\\CGMLessonProject\\Documenti"); 
     if(!documentiDir.exists()) { 
         documentiDir.mkdirs();
-    } // make directories } 
-    File testo = new File(documentiDir, "readme.txt"); 
+    } 
+    //CREO IL MIO FILE DI TESTO
+    File testo = new File(documentiDir, "testo.txt"); 
     if(!testo.exists()){
         testo.createNewFile();
     } 
@@ -27,6 +32,8 @@ public class FileMain2 {
         readme.createNewFile(); 
     } 
     
+    //FACCIO UNO OUTSTREAM SU README
+    
     byte[] datas = {99, 98, 97};
     OutputStream outputStream = new FileOutputStream(readme);
     
@@ -35,5 +42,22 @@ public class FileMain2 {
     outputStream.write((byte) 97); 
     outputStream.flush(); 
     outputStream.close(); 
+    
+    //FACCIO UN INPUNTSTREAM SU README
+    
+    final int fileL= (int) readme.length();
+    
+    byte[] moredatas=new byte[fileL];
+    InputStream inputStream= new FileInputStream(readme);
+    
+    int data;
+    int i=0;
+
+    while((data= inputStream.read()) !=-1){
+        moredatas[i]= (byte) data;
+        i++;
+    }
+    inputStream.close();
+    System.out.println(Arrays.toString(moredatas));
     }
 }
