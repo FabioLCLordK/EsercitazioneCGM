19b4cbe8f2a710c9e8f6a2dc60ed5e95404b3357
LordKazor <LordKazor@KazorCaverna2.mshome.net>
2023-03-06 11:00:15 +0100

    Rev 2.2 06/03

--- src/main/java/teoryLombok/User.java
+++ src/main/java/teoryLombok/User.java
@@ -1,11 +1,15 @@
 package teoryLombok;
 
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     private String name;
     private String surname;
